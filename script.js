bc77da109618f63a4ae9f0921d395510e54bab4d
Tallia-Anne <erinawambiekele@gmail.com>
2022-03-20 20:08:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// animation qui fait défiler le bouton
+document.querySelector('.img-btn').addEventListener('click', function() {
+    document.querySelector('.cont').classList.toggle('s-signup')
+});
